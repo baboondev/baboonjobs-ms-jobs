eb5df4b9ad759b163d16d23eb921173f9cfa66b0
Nicolas Caro <niicolas.caro@gmail.com>
2021-10-25 19:08:24 -0300

    Initialize spring

---
+++ src/main/java/com/baboondev/baboonjobsmsjobs/BaboonjobsMsJobsApplication.java
@@ -0,0 +1,13 @@
+package com.baboondev.baboonjobsmsjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaboonjobsMsJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaboonjobsMsJobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baboondev/baboonjobsmsjobs/BaboonjobsMsJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baboondev.baboonjobsmsjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaboonjobsMsJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
